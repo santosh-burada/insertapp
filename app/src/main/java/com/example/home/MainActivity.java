a4c581e87dab25b70fa06e9048b855e5a7e806e0
santoshsampath <santu.burada99@gmail.com>
2020-04-11 10:52:47 +0530

    insertion app

---
+++ app/src/main/java/com/example/home/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.home;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+    TextView sms;
+    Button record , stop;
+    DatabaseReference reff;
+    String myvalue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        record=findViewById(R.id.record);
+        stop=findViewById(R.id.stop);
+        sms=findViewById(R.id.sms);
+        reff= FirebaseDatabase.getInstance().getReference().child("recorder");
+        record.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reff.child("home1").setValue("1");
+                sms.setText("inserted");
+
+            }
+        });
+        reff.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                myvalue=dataSnapshot.getValue(String.class);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, ""+myvalue, Toast.LENGTH_SHORT).show();
+                //reff.removeValue();
+            }
+        });
+
+    }
+}
